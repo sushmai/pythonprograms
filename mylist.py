70f7ce87b60fa6bacb41ab9de9c34049ecdb9e34
sushmai <maryadareddy@gmail.com>
2016-10-19 22:13:30 -0700

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -1,5 +1,22 @@
-myList = [1,2,3,4]
-A = [myList]*3
-print(A)
-myList[2]=45
-print(A)
+myList = [1024, 3, True, 6.5]
+myList.append(False)
+print(myList)
+myList.insert(2,4.5)
+print(myList)
+print(myList.pop())
+print(myList)
+print(myList.pop(1))
+print(myList)
+myList.pop(2)
+print(myList)
+myList.sort()
+print(myList)
+myList.reverse()
+print(myList)
+print(myList.count(6.5))
+print(myList.index(4.5))
+myList.remove(6.5)
+print(myList)
+del myList[0]
+print(myList)
+
