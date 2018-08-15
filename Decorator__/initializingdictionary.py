c6c79aa48f02f62bac6bfe6cfe8b000896a78730
sushmai <maryadareddy@gmail.com>
2018-08-14 17:30:08 -0700

    Add files via upload

---
+++ Decorator__/initializingdictionary.py
@@ -0,0 +1,33 @@
+s = 'a a a a a a a a a a a a a a '
+
+words = s. split()
+d = {}. fromkeys(words, 0)
+for w in words:
+    d[w] += 1
+
+print (d)
+
+"""
+a = "hello world this is python code second line"
+d = {}
+for w in a.split():
+    d[w] = d.get(w,0) + 1
+print (d)
+
+
+s = """
+"""I figured it out
+i figured it out from balck and white
+seconds and minutes
+nothing to take
+"""
+"""
+
+from collections import defaultdict
+d =  defaultdict(int)
+
+for w in words:
+    d[w] += 1
+
+print (d)
+"""
