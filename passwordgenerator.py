a863d9b529641c2b25b09e6be589eebaaab3fc16
sushmai <maryadareddy@gmail.com>
2018-08-21 15:12:08 -0700

    Add files via upload

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,10 +1,13 @@
 import random
 import string
 password = []
-a = []
+#a = []
+#a = str(input("do you want 'strong' or 'weak' password ? "))
+
 def generate(password):
+    #a = []
     a = str(input("do you want 'strong' or 'weak' password ? "))
-    password = []
+    #password = []
 
     upper = string.ascii_uppercase
     lower = string.ascii_lowercase
@@ -12,15 +15,19 @@ def generate(password):
     number = string.digits
     everything = upper + lower + special + number
 
-    if a == "strong" :
-        password = random.sample(everything, 12)
-        return("".join(password))
-    elif a == "weak":
-        password = random.sample(everything, 6)
-        return("".join(password))
-    else:
-        print(a)
-    return
+    while len(password) != (6 or 12):
+        if a == "strong" :
+            password = random.sample(everything, 12)
+            return("".join(password))
+
+        elif a == "weak":
+        
+            password = random.sample(everything, 6)
+            return("".join(password))
+        else:
+            a = str(input("select again "))
+            #print(f"Please select only from..'strong' or 'weak'")
+            #return
 print((generate(password)))
-# print(a)
+#print(a)
 #else case is not working
