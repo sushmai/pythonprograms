4494dba640add973689b1269ebb6e197c0be8456
sushmai <maryadareddy@gmail.com>
2018-08-23 15:10:59 -0700

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,5 @@
+# next number is sum of previous two numbers (1,1,2,3,5,8,13,21..... n )
+"""
 a = int(input("Enter a number :"))
 if a == 1:
     b = [1]
@@ -32,3 +34,75 @@ def Fibonacci(num):
         print("invalid")
 num = int(input("enter the number:"))
 Fibonacci(num)
+
+# using function 
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n > 2:
+        return fibonacci(n-1) + fibonacci(n-2)
+for n in range(1,11):
+    # each time it will call the all values (calcultes again and againg memory and performance issue)
+    print(n, ':', fibonacci(n))
+
+# using memoization :
+# 1. Implement explicitely to see how it works :
+# dictionary to srote recent function calls (fibonacci values)
+fibonacci_cache = {}
+def fibonacci(n):
+    #if we have cached the value then return it
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+    # compute the nth term
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2 :
+        value = fibonacci(n-1) + fibonacci(n-2)
+    #cache the value and return it
+    fibonacci_cache[n] = value
+    return value
+for n in range(1,30):
+    print(n, ':', fibonacci(n))
+"""
+# 2. Built in python tool that makes momoization trivial
+from functools import lru_cache
+@lru_cache(maxsize = 1000)# default 1028 values
+def fibonacci(n):
+    # doesn't work for float or -ve points or string
+    # so give user feedback
+    #check that the input is positive integer
+    if type(n) != int:
+        raise TypeError("n must be a positive integer")
+    if n < 1:
+        raise TypeError("n must be a positive integer")
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(10))
+
+"""
+# doesn't work for float or -ve points or string 
+for n in range(1, 34):
+    print(n, ":", fibonacci(n))
+    
+"""   
+
+
+
+
+
+
+
+
+
+
+
+    
+    
