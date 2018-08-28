661b3ac4168da3be94ea31110387f75c2edca94c
sushmai <maryadareddy@gmail.com>
2018-08-27 23:54:31 -0700

    Add files via upload

---
+++ my_stuff_module.py
@@ -0,0 +1,12 @@
+#we can import my_stuff_mudule with apple function for future use
+
+def apple():
+    print("This just prints 'apple'")
+
+
+# call th fuction:
+#apple()
+
+# imports variable as well, now create a variable :
+
+variable = "This will be printed when the module calls"
