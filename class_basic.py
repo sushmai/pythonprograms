d56ca5687d2400c823e147e62de454e29723d536
sushmai <maryadareddy@gmail.com>
2018-08-28 00:09:10 -0700

    Add files via upload

---
+++ class_basic.py
@@ -0,0 +1,19 @@
+class Song(object):
+    
+    def __init__(self, lyrics):
+
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Happy Birthday to you..",
+                   "I will stop here...."])
+
+bulls_on_parade = Song(["They rally around the house",
+                        "With something"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
