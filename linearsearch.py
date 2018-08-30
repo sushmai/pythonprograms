4167bd01786a331b8796918ad4e7a60d8aef4c19
sushmai <maryadareddy@gmail.com>
2018-08-29 20:36:42 -0700

    Add files via upload

---
+++ linearsearch.py
@@ -0,0 +1,16 @@
+"""def linearSearch(ls, data):
+
+    for item in ls :
+        if item == data:
+            return True
+    return False
+print (linearSearch([6,3,9,8,2], 0))
+
+"""
+def linearSearch (ls, data):
+
+    for item in ls:
+        if item == data:
+            return True
+    return False
+print (linearSearch([5,3,0,4,3,1],0))
---
+++ strip.py
@@ -0,0 +1,13 @@
+sample = "    python    "
+print(sample.strip())
+
+
+hello = "   hell   o "
+print(hello.strip())
+
+
+number = "  23322  121334"
+print(number.strip())
+
+notstrip = "Hello World"
+print(notstrip.strip())
