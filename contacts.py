43843f09fefa26ffc286e5ca695e9b753a2221da
sushmai <maryadareddy@gmail.com>
2018-09-15 23:52:08 -0700

    Add files via upload

---
+++ contacts.py
@@ -0,0 +1,45 @@
+def main():
+    print("Contacts Manager:")
+    
+
+    #initialize friends list
+    friends = []
+
+    choice = 0
+    while choice != 4:
+        print("1 Add a friend:")
+        print("2 Lookup contacts")
+        print("3 display all contacts")
+        print("4 Quiit" )
+
+        choice = eval(input())
+
+        if choice == 1:
+            print("Adding a friend")
+            name = input("Enter a name:")
+            email = input("enter email:")
+            phone = input("enter phone:")
+            friends.append([name, email, phone])
+            
+        elif choice == 2:
+            print("Look up a friend ")
+            keyword = input("Enter name to search: ")
+            for friend in friends:
+                if keyword in friend:
+                    print(friend)
+            
+        elif choice == 3:
+            print("Displaying all contacts")
+            for i in range(len(friends)):
+                print(friends[i])
+        elif choice == 4:
+            print("End the program")
+        else:
+            print("Invalid response..")
+    print("Program terminated ")
+
+
+
+
+
+main()
