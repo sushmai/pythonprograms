26d4acc417e10a94580a493427e6495bd5322c42
sushmai <maryadareddy@gmail.com>
2018-09-18 22:39:20 -0700

    Add files via upload

---
+++ python_basic_syntax.py
@@ -0,0 +1,161 @@
+# comments
+# this is a on line pythin comment - code blocks are useful!!
+"""
+This helps to document the purpose of functions and classes.
+
+"""
+
+# Declaration / Initialization
+answer = 32
+answer = "The answer is 32."
+
+# DataTypes
+boolean = True
+string = "String can be declared with single or double 'quotes'"
+list = ['lists can have', 1,2,3,4,"or more types together!"]
+tuple = ("Tuples", "can have" "More than", 2, " elements")
+dictionary = {"one":1, "two":2, "three":3}
+
+variables_with_zero_data = None
+
+# Simple Logging :
+print("printed")
+
+
+# Conditions :
+if cake == "Delicious":
+    return("Yes please!")
+elif cake == "Okay":
+    return( "Let's try...")
+else:
+    return("no, Thank you ")
+
+# Loops
+
+for item in list :
+    print item
+
+while (total < max_val):
+    total += values[i]
+    i +=2
+
+# Functions
+
+def divide(dividend, divisior):
+    quotient = dividend / divisor
+    reminder = dividend % divisor
+    return quotent, reminder
+
+def calculate_stuff(x,y):
+    (q,r) = divide(x,y)
+    print(q,r)
+
+# classes
+class Person(object):
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        self.age += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
