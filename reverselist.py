7776cb499c24664fd002d07f0e3b20c766e4bc85
sushmai <maryadareddy@gmail.com>
2018-09-22 23:22:53 -0700

    Add files via upload

---
+++ reverselist.py
@@ -0,0 +1,15 @@
+def reverselist(list):
+    prev = None
+    curr = list.head
+    nex = curr.getNextNode()
+
+    while curr:
+        curr.setNextNode(prev)
+
+        prev = curr
+        curr = nex
+        if nex:
+            nex = nex.getNextNode()
+
+    list.head = prev
+        
